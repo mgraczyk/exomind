8e89b406e4b4431bddd93371befd8059de717b37
Michael Graczyk <michael@mgraczyk.com>
2019-09-11 13:28:47 -0700

    fix edit review url

--- app/urls.py
+++ app/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('logout', sv.logout_view, name='logout'),
     path('feed', sv.feed_view, name='feed'),
     path('reviews/new', sv.manage_review_view, name='new_review'),
-    path('reviews/<b64id:review_id>', sv.manage_review_view, name='manage_review'),
-    path('reviews/<b64id:review_id>/edit', sv.full_review_view, name='full_review'),
+    path('reviews/<b64id:review_id>/edit', sv.manage_review_view, name='manage_review'),
+    path('reviews/<b64id:review_id>', sv.full_review_view, name='full_review'),
     path('profiles/<uuid:user_id>', sv.profile_view, name='profile_view'),
 
     path('admin', av.admin_view, name='admin'),
